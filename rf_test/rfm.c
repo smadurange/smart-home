8c7912f49ec7ec17e79203214240eadd5eec00f0
Sadeep Madurange <sadeep@asciimx.com>
2024-11-24 12:56:52 +0800

    Rename local var.

--- rf_test/rfm.c
+++ rf_test/rfm.c
@@ -109,16 +109,16 @@ void rfm_sendto(uint8_t addr, uint8_t *data, uint8_t n)
 
 uint8_t rfm_recvfrom(uint8_t addr, uint8_t *buf, uint8_t n)
 {
-	uint8_t i, bytes;
+	uint8_t i, buflen;
 	
 	SS_PORT |= (1 << SS_PIN);
 
 	for (i = 0; i < n && ((read_reg(0x28) >> 6) & 1); i++) {
 		buf[i] = read_reg(0x00);
-		bytes++;
+		buflen++;
 	}
 
 	SS_PORT &= ~(1 << SS_PIN);
 
-	return bytes;
+	return buflen;
 }
