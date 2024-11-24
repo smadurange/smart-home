1b486a6f7fd82f923acbfdc7b03f2fc6b0ec773d
Sadeep Madurange <sadeep@asciimx.com>
2024-11-24 12:56:17 +0800

    Return read byte count.

--- rf_test/rfm.c
+++ rf_test/rfm.c
@@ -107,14 +107,18 @@ void rfm_sendto(uint8_t addr, uint8_t *data, uint8_t n)
 		;
 }
 
-void rfm_recvfrom(uint8_t addr, uint8_t *buf, uint8_t n)
+uint8_t rfm_recvfrom(uint8_t addr, uint8_t *buf, uint8_t n)
 {
-	uint8_t i;
+	uint8_t i, bytes;
 	
 	SS_PORT |= (1 << SS_PIN);
 
-	for (i = 0; i < n && ((read_reg(0x28) >> 6) & 1); i++)
+	for (i = 0; i < n && ((read_reg(0x28) >> 6) & 1); i++) {
 		buf[i] = read_reg(0x00);
+		bytes++;
+	}
 
 	SS_PORT &= ~(1 << SS_PIN);
+
+	return bytes;
 }
