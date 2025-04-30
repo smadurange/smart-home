3e0215f1bcb7018dbbcd1cff9b2dce606cb0328d
Sadeep Madurange <sadeep@asciimx.com>
2025-04-30 11:15:25 +0800

    Reduce sleep time to 1 second.

--- lock/bend.c
+++ lock/bend.c
@@ -70,7 +70,7 @@ static inline void init_wdt(void)
 	cli();
 	wdt_reset();
 	WDTCSR |= (1 << WDCE) | ( 1 << WDE); 
-	WDTCSR = (1 << WDP2) | (1 << WDP1) | (1 << WDP0);
+	WDTCSR = (1 << WDP2) | (1 << WDP1);
 	WDTCSR |= (1 << WDIE);
 }
 
